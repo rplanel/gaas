abb11f9feb5d5e305f2b527a06ed2afc812636cd
Rémi Planel <rplanel@gmail.com>
2025-01-01 22:13:34 +0100

    configure monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default antfu(
   {
     type: 'lib',
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+
   },
 )
