c0920132a02cf76f7650ce35c7a4866127848445
Remi  PLANEL <rplanel@pasteur.fr>
2025-01-16 16:00:22 +0100

    Make all children page of [analysisId] have the analysis summary

--- packages/gaas-ui/app/pages/analyses/[analysisId].vue
+++ packages/gaas-ui/app/pages/analyses/[analysisId].vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
 import type { SupabaseTypes } from '#build/types/database'
-import type { BreadcrumbItem } from '@nuxt/ui'
+import type { GalaxyTypes } from '#build/types/nuxt-galaxy'
+import type { AccordionItem, BreadcrumbItem } from '@nuxt/ui'
+import type { GalaxyTool, GalaxyToolParameters } from 'blendtype'
+import { useGalaxyDecodeParameters } from '../../composables/galaxy/useGalaxyDecodeParameters'
+import { type GalaxyToolInputComponent, useGalaxyToolInputComponent } from '../../composables/galaxy/useGalaxyToolInputComponent'
 
 type Database = SupabaseTypes.Database
+type AnalysisDetail = GalaxyTypes.AnalysisDetail
+export type InputDatasets = typeof inputs.value
+export type OutputDatasets = typeof outputs.value
+type RowAnalysisJob = GalaxyTypes.RowAnalysisJob
+type AnalysisOutputsWithDatasets = GalaxyTypes.AnalysisOutputsWithDatasets
 
 interface Props {
   breadcrumbsItems?: BreadcrumbItem[] | undefined
 }
 const props = withDefaults(defineProps<Props>(), { breadcrumbsItems: undefined })
+
+definePageMeta({
+  middleware: 'auth',
+  layout: 'toc',
+})
+
 const { breadcrumbsItems } = toRefs(props)
 const route = useRoute()
 const supabase = useSupabaseClient<Database>()
 const user = useSupabaseUser()
-
+const workflowParametersModel = ref<
+  | Record<string, Record<string, string | string[] | Record<string, any>>>
+  | undefined
+>(undefined)
 const analysisId = computed(() => {
   const analysisId = route?.params?.analysisId
   if (Array.isArray(analysisId))
@@ -23,48 +41,92 @@ const analysisId = computed(() => {
   return analysisId
 })
 
+const { tools, getToolParameters, getParametersInputComponent } = useAnalysisTools()
+const { inputs } = useAnalysisInput()
+const { jobs, jobsAccordionItems, jobsMap, jobDetailsAccordionItems } = useAnalysisJob()
+const { workflowSteps } = useAnalysisWorkflow()
+const { outputs } = useOutputs()
+// Fetch data
+
 const { data: analysis } = await useAsyncData(
-  `analysis-${toValue(analysisId)}`,
+  `analysis-details-${toValue(analysisId)}`,
   async () => {
-    const analysisIdVal = toValue(analysisId)
+    const analysisVal = toValue(analysisId)
     const userVal = toValue(user)
+    if (userVal && analysisVal) {
+      const { data, error } = await supabase
+        .schema('galaxy')
+        .from('analyses')
+        .select(
+          `
+        *,
+        histories(*),
+        jobs(*),
+        workflows(*),
+        analysis_inputs(
+            *,
+            datasets(*)
+        ),
+        analysis_outputs(
+            *,
+            datasets(*)
+        )
+    `,
+        )
+        .eq('id', analysisVal)
+        .limit(1)
+        .returns<AnalysisDetail[]>()
 
-    if (userVal === null) {
-      throw createError({
-        statusMessage: 'User not found',
-        statusCode: 404,
-      })
+      if (error) {
+        throw createError({
+          statusMessage: error.message,
+          statusCode: Number.parseInt(error.code),
+        })
+      }
+      return data ? data[0] : data
     }
-    if (analysisIdVal === undefined) {
-      throw createError({
-        statusMessage: 'Analysis not found',
-        statusCode: 404,
-      })
-    }
-    const { data, error } = await supabase
-      .schema('galaxy')
-      .from('analyses')
-      .select()
-      .eq('id', analysisIdVal)
-      .limit(1)
-      .returns<Database['galaxy']['Tables']['analyses']['Row'][]>()
-
-    if (error) {
-      throw createError({
-        statusMessage: error.message,
-        statusCode: Number.parseInt(error.code),
-      })
-    }
-    if (data === null) {
-      throw createError({
-        statusMessage: '',
-        statusCode: 500,
-      })
-    }
-    return data[0]
+    return false
   },
-
 )
+
+const { data: dbWorkflow } = await useAsyncData('workflow-db', async () => {
+  const userVal = toValue(user)
+  const analysisVal = toValue(analysis)
+  if (!userVal) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized: User not found',
+    })
+  }
+  if (!analysisVal) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Not Found: Analysis not found',
+    })
+  }
+  const workflowIdVal = toValue(analysisVal.workflow_id)
+  const { data } = await supabase
+    .schema('galaxy')
+    .from('workflows')
+    .select('id, name, galaxy_id, definition')
+    .eq('id', workflowIdVal)
+    .limit(1)
+    .single()
+  return data
+})
+
+const workflowGalaxyId = computed(() => {
+  const dbWorkflowVal = toValue(dbWorkflow)
+  if (dbWorkflowVal)
+    return dbWorkflowVal.galaxy_id
+  return undefined
+})
+
+const { data: workflowRun } = await useFetch(
+  `/api/galaxy/workflows/${toValue(workflowGalaxyId)}/input`,
+)
+
+// computed data
 
 const computedBreadcrumbsItems = computed(() => {
   const analysisVal = toValue(analysis)
@@ -81,10 +143,315 @@ const computedBreadcrumbsItems = computed(() => {
   }
   return breadcrumbsItemsVal
 })
+
+const history = computed(() => {
+  const analysisVal = toValue(analysis)
+  if (analysisVal && analysisVal.histories) {
+    return analysisVal.histories
+  }
+  return undefined
+})
+
+function useAnalysisInput() {
+  const inputs = computed(() => {
+    const analysisVal = toValue(analysis)
+    if (analysisVal && analysisVal?.analysis_inputs) {
+      return analysisVal.analysis_inputs.map((input) => {
+        return {
+          ...input.datasets,
+          state: input.state,
+        }
+      })
+    }
+    return undefined
+  })
+
+  return { inputs }
+}
+
+function useAnalysisJob() {
+  const jobs = computed<RowAnalysisJob[] | undefined>(() => {
+    const analysisVal = toValue(analysis)
+    if (analysisVal && analysisVal?.jobs) {
+      return analysisVal.jobs
+    }
+    return undefined
+  })
+
+  const jobsAccordionItems = computed<AccordionItem[] | undefined>(() => {
+    const jobsVal = toValue(jobs)
+    const toolsVal = toValue(tools)
+    if (jobsVal && toolsVal) {
+      return jobsVal.map((job): AccordionItem => {
+        return {
+          label: `${toolsVal[job.tool_id]?.name ?? 'no tool name'} - ${
+            toolsVal[job.tool_id]?.version ?? 'no tool version'
+          }`,
+          icon: 'i-mdi:tools',
+          value: String(job.step_id),
+        }
+      })
+    }
+    return undefined
+  })
+
+  const jobsMap = computed(() => {
+    const jobsVal = toValue(jobs) as RowAnalysisJob[]
+    if (jobsVal) {
+      const jobM: Record<string, RowAnalysisJob> = {}
+      for (const job of jobsVal) {
+        jobM[String(job.step_id)] = job
+      }
+      return jobM
+    }
+    return {}
+  })
+
+  const jobDetailsAccordionItems = computed(() => {
+    const jobsVal = toValue(jobs)
+    const perJobItems: Record<string, { details: AccordionItem[] }> = {}
+    if (jobsVal) {
+      for (const job of jobsVal) {
+        perJobItems[job.step_id] = {
+          details: [
+            { label: 'Parameters', slot: 'parameters' },
+            { label: 'Stdout', slot: 'stdout' },
+            { label: 'Stderr', slot: 'stderr' },
+          ],
+        }
+      }
+      return perJobItems
+    }
+    return undefined
+  })
+
+  return { jobs, jobsAccordionItems, jobsMap, jobDetailsAccordionItems }
+}
+
+function useAnalysisWorkflow() {
+  const workflowSteps = computed(() => {
+    const workflowRunVal = toValue(workflowRun)
+    if (workflowRunVal?.galaxyWorkflow?.steps) {
+      return workflowRunVal.galaxyWorkflow.steps
+    }
+    return undefined
+  })
+
+  return { workflowSteps }
+}
+
+function useAnalysisTools() {
+  const tools = computed(() => {
+    const workflowRunVal = toValue(workflowRun)
+    if (workflowRunVal?.tools) {
+      return workflowRunVal.tools
+    }
+    return {}
+  })
+
+  const stepTools = computed(() => {
+    const workflowRunVal = toValue(workflowRun)
+    if (workflowRunVal?.stepToTool) {
+      return workflowRunVal.stepToTool
+    }
+    return {}
+  })
+  const sanitizedToolsParameters = computed(() => {
+    const workflowRunVal = toValue(workflowRun)
+    if (workflowRunVal) {
+      const toolsInputs: Record<string, GalaxyToolParameters[]> = {}
+      for (const toolId in workflowRunVal.tools) {
+        if (workflowRunVal.tools[toolId]?.inputs) {
+          toolsInputs[toolId] = workflowRunVal.tools[toolId]?.inputs.filter(
+            input => input.type !== 'data',
+          ) as GalaxyToolParameters[]
+        }
+      }
+      return toolsInputs
+    }
+    return undefined
+  })
+  function getToolParameters(stepId: string) {
+    const stepToolsVal = toValue(stepTools)
+    const sanitizedToolsParametersVal = toValue(sanitizedToolsParameters)
+    const toolName = stepToolsVal[stepId]
+    if (toolName && sanitizedToolsParametersVal) {
+      return sanitizedToolsParametersVal[toolName]
+    }
+  }
+
+  const computedParameterInputComponentObject = computed(() => {
+    const worklowRunVal = toValue(workflowRun)
+    if (worklowRunVal?.tools) {
+      return Object.entries(worklowRunVal.tools).reduce(
+        (
+          acc: Record<string, Record<string, GalaxyToolInputComponent>>,
+          curr,
+        ) => {
+        // toolInput.
+          const [toolId, tool] = curr as [string, GalaxyTool]
+          const { inputComponentsObject } = useGalaxyToolInputComponent(
+            tool.inputs,
+          )
+          if (inputComponentsObject.value)
+            acc[toolId] = inputComponentsObject.value
+          return acc
+        },
+        {} as Record<string, Record<string, GalaxyToolInputComponent>>,
+      )
+    }
+    return undefined
+  })
+  function getParametersInputComponent(stepId: string) {
+    const toolName = toValue(stepTools)[stepId]
+    const computedParameterInputComponentObjectVal = toValue(computedParameterInputComponentObject)
+    if (toolName && computedParameterInputComponentObjectVal) {
+      return computedParameterInputComponentObjectVal[toolName]
+    }
+  }
+
+  return { tools, getToolParameters, getParametersInputComponent }
+}
+
+function useOutputs() {
+  const outputs = computed(() => {
+    const analysisVal = toValue(analysis)
+    if (analysisVal && analysisVal?.analysis_outputs) {
+      return analysisVal.analysis_outputs.map((output: AnalysisOutputsWithDatasets) => {
+      // const { fileSize } = useFileSize(output.datasets.file_size);
+        return {
+          ...output.datasets,
+          state: output.state,
+        // humanFileSize: fileSize,
+        }
+      })
+    }
+    return undefined
+  })
+
+  return { outputs }
+}
+
+onMounted(() => {
+  const dbAnalysisVal = toValue(analysis) as Record<string, any>
+
+  const { decodedParameters } = useGalaxyDecodeParameters(
+    dbAnalysisVal.parameters,
+  )
+  workflowParametersModel.value = toValue(decodedParameters)
+})
 </script>
 
 <template>
   <div>
+    <PageHeader
+      v-if="analysis" :title="analysis.name" description="Analysis perform with workflow"
+      icon="i-streamline:code-analysis" :breadcrumbs-items="breadcrumbsItems"
+    >
+      <template #description="{ description }">
+        <div class="text-lg text-[var(--ui-text-muted)] mt-4">
+          {{ description }}
+          <UBadge variant="subtle">
+            {{
+              workflowRun?.galaxyWorkflow.name
+            }}
+          </UBadge>
+        </div>
+      </template>
+      <template #trailing-content>
+        <div v-if="history">
+          <GalaxyStatus :state="history.state" :size="40" />
+        </div>
+      </template>
+    </PageHeader>
+    <USeparator icon="i-lucide:file" />
+    <div class="py-4">
+      <h2 class="text-lg font-bold">
+        Inputs
+      </h2>
+      <GalaxyAnalysisIoDatasets :items="inputs" />
+    </div>
+
+    <USeparator icon="i-mdi:tools" />
+
+    <div v-if="jobs" name="job">
+      <div class="py-4">
+        <h2 class="text-lg font-bold">
+          Jobs
+        </h2>
+      </div>
+
+      <UAccordion
+        :items="jobsAccordionItems" :ui="{
+          header:
+            'flex hover:bg-[var(--ui-bg-elevated)] px-2 rounded-[calc(var(--ui-radius))] grid grid-flow-col align-middle',
+          trailingIcon: 'shrink-1 shrink-none',
+        }"
+      >
+        <template #leading="{ item }">
+          <div>
+            <GalaxyStatus :state="item?.value && jobsMap ? jobsMap[item.value]?.state : undefined" size="25" />
+          </div>
+        </template>
+        <template #body="{ item }">
+          <!-- item.value is step_id as string -->
+          <div v-if="jobDetailsAccordionItems && item.value" class="p-4">
+            <UAccordion
+              :items="jobDetailsAccordionItems[item.value]?.details" :ui="{
+                header:
+                  'hover:bg-[var(--ui-bg-elevated)] px-2 rounded-[calc(var(--ui-radius))]',
+              }"
+            >
+              <template #parameters>
+                <div
+                  v-if="
+                    workflowParametersModel
+                      && workflowSteps
+                      && item.value
+                  " class="p-2"
+                >
+                  <div class="ring ring-[var(--ui-border)] rounded-[calc(var(--ui-radius)*2)]">
+                    <GalaxyWorkflowStep
+                      variant="display"
+                      :workflow-step="workflowSteps[item.value]"
+                      :tool-parameters="getToolParameters(item.value)"
+                      :parameters-inputs-component="getParametersInputComponent(item.value)"
+                      :workflow-parameters-model=" workflowParametersModel[item.value] "
+                    />
+                  </div>
+                </div>
+              </template>
+              <template #stdout>
+                <div class="p-1">
+                  <div class="ring ring-[var(--ui-border)] rounded-[calc(var(--ui-radius)*2)] p-2">
+                    <pre v-if="jobsMap"> {{ jobsMap[item.value]?.stdout }}</pre>
+                  </div>
+                </div>
+              </template>
+              <template #stderr>
+                <div class="p-1">
+                  <div class="ring ring-[var(--ui-border)] rounded-[calc(var(--ui-radius)*2)] p-2">
+                    <pre v-if="jobsMap"> {{ jobsMap[item.value]?.stderr }}</pre>
+                  </div>
+                </div>
+              </template>
+            </UAccordion>
+          </div>
+        </template>
+      </UAccordion>
+    </div>
+
+    <!-- outputs -->
+    <USeparator icon="i-lucide:file" />
+    <div v-if="outputs">
+      <div class="py-4">
+        <h2 class="text-lg font-bold">
+          Outputs
+        </h2>
+        <GalaxyAnalysisIoDatasets :items="outputs" />
+      </div>
+    </div>
+
     <NuxtPage :breadcrumbs-items="computedBreadcrumbsItems" :analysis-id="analysisId" />
   </div>
 </template>
