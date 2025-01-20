5a1834ab01434a4c3abbe52da06717c9c0459d07
Remi  PLANEL <rplanel@pasteur.fr>
2025-01-20 16:10:35 +0100

    playground nuxt 4 compat

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   extends: ['../packages/gaas-ui'],
   compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
 })
