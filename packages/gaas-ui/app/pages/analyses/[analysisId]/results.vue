c18932e7174a5fb458f2f7f2d4c53506e3051718
Remi  PLANEL <rplanel@pasteur.fr>
2025-01-21 23:19:48 +0100

    add separtor

--- packages/gaas-ui/app/pages/analyses/[analysisId]/results.vue
+++ packages/gaas-ui/app/pages/analyses/[analysisId]/results.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="p-5">
-    <UAlert icon="mdi:message-alert" color="warning" variant="subtle" title="Not implemented" description="The result page is not implemented. This is not the function of this layer" />
+  <div>
+    <USeparator icon="tabler:chart-scatter" />
+    <div class="py-4">
+      <h2 class="text-lg font-bold">
+        Results
+      </h2>
+      <UAlert icon="mdi:message-alert" color="warning" variant="subtle" title="Not implemented" description="The result page is not implemented. This is not the function of this layer" class="my-3" />
+    </div>
   </div>
 </template>
